73873b8293e69c0c4e8e0ba25e00f08f7e08e97a
sksumayya <sksumayya10@gmail.com>
2024-02-15 11:13:22 +0530

    first commit today

---
+++ project1.R
@@ -0,0 +1,9 @@
+Information gathering:
+  Information gathering, as the name suggests, is all about gathering information about the target system or network. It is the first and one of the most crucial steps in penetration testing. Here, the pen testers try to find as much information as possible about the target.
+We use various tools to gather the information like netcraft , wayback machine and submarine cable map.
+Netcraft:
+  Netcraft's online brand protection operates 24/7 to discover phishing, fraud, scams, and cyber attacks through extensive automation, AI, machine learning, and human insight. Our disruption & takedown service ensures that malicious content is blocked and removed quickly and efficiently—typically within hours.it gathers the information about the IP addresses of the link given as input to it
+  STEP-1:
+Open command prompt
+We use ping command to troubleshoot connectivity , reachability and name resolution.
+Here is the screenshot of usage of ping command
