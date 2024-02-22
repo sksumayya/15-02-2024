3bc19c01f000f9b543d2978fdf0843bc745fc4cb
sksumayya <sksumayya10@gmail.com>
2024-02-22 11:21:03 +0530

    vectors

---
+++ vectors.R
@@ -0,0 +1,26 @@
+numeric_vector <- c(1, 2, 3, 4, 5)
+character_vector <- c("apple", "orange", "banana")
+logical_vector <- c(TRUE, FALSE, TRUE)
+numeric_vector[3]
+character_vector[2] 
+vec1 <- c(1, 2, 3)
+vec2 <- c(4, 5, 6)
+result_addition <- vec1 + vec2
+print(result_addition)
+result_multiplication <- vec1 * vec2
+print(result_multiplication)
+vec1 <- c(1, 2, 3)
+vec2 <- c(3, 2, 1)
+result_equal <- vec1 == vec2
+print(result_equal)
+result_greater_than <- vec1 > vec2
+print(result_greater_than)
+numeric_vector <- c(1, 2, 3, 4, 5)
+result_sum <- sum(numeric_vector)
+print(result_sum)
+result_mean <- mean(numeric_vector)
+print(result_mean)
+vec1 <- c(1, 2, 3)
+vec2 <- c(4, 5, 6)
+result_concatenation <- c(vec1, vec2)
+print(result_concatenation)
